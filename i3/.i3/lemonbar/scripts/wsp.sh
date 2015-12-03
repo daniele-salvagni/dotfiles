6b8be514197ecabc638496cb337ad627364ee5e9
Daniele Salvagni <daniele.salvagni@gmail.com>
2015-12-03 20:34:35 +0100

    Change separator and remove useless output

--- i3/.i3/lemonbar/scripts/wsp.sh
+++ i3/.i3/lemonbar/scripts/wsp.sh
@@ -2,16 +2,16 @@
 
 # WORKSPACES
 #
-# Output format ([,],|,$ characters are not part of the output):
-# WSP: %{workspaces:$wmctrl-output}%
+# Output format:
+# WSP: %{workspaces:%[-1Ws1]%%[*2Ws2]%}%
 
 while :
 do
 
-  # Put the output of wmctrl on one line, lines will be separated by '-%-'
-  workspaces=$(wmctrl -d | awk '{print $0}' ORS='-%-')
+  # Put the output of wmctrl on one line, lines will be separated by '#%#'
+  workspaces=$(wmctrl -d | awk '{print $2 substr($0, index($0,$9))}' ORS='#%#')
 
-  # Note: to put it back on multiple lines | awk '{print $0}' RS='-%-'
+  # Note: to put it back on multiple lines | awk '{print $0}' RS='#%#'
 
   echo "WSP:%{workspaces:$workspaces}%"
 
