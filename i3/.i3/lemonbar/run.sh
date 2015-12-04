bf144c12ce9d64cc1064c6bc0646ecf1d4230545
Daniele Salvagni <daniele.salvagni@gmail.com>
2015-12-04 12:51:35 +0100

    Reflect folder name change to 'components'

--- i3/.i3/lemonbar/run.sh
+++ i3/.i3/lemonbar/run.sh
@@ -10,12 +10,12 @@ fifo="/tmp/.fifo"
 mkfifo $fifo
 
 xtitle -sf 'T%s' > $fifo &
-/home/dan/.i3/lemonbar/scripts/bat.sh > $fifo &
-/home/dan/.i3/lemonbar/scripts/dat.sh > $fifo &
-/home/dan/.i3/lemonbar/scripts/net.sh > $fifo &
-/home/dan/.i3/lemonbar/scripts/thc.sh > $fifo &
-/home/dan/.i3/lemonbar/scripts/vol.sh > $fifo &
-/home/dan/.i3/lemonbar/scripts/wsp.sh > $fifo &
+/home/dan/.i3/lemonbar/components/bat.sh > $fifo &
+/home/dan/.i3/lemonbar/components/dat.sh > $fifo &
+/home/dan/.i3/lemonbar/components/net.sh > $fifo &
+/home/dan/.i3/lemonbar/components/thc.sh > $fifo &
+/home/dan/.i3/lemonbar/components/vol.sh > $fifo &
+/home/dan/.i3/lemonbar/components/wsp.sh > $fifo &
 
 cat $fifo | .i3/lemonbar/bar.py | lemonbar -n bar-main -p -B#982c2c -F#b49f85 -g 1600x20+0+0 -f 'tewi:pixelsize=10:antialias=true' -f 'siji:pixelsize=11' | bash
 
