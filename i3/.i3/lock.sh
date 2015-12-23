05314123c97a013bcaeb260ef8f885a0ba2f9ae4
Daniele Salvagni <daniele.salvagni@gmail.com>
2015-12-23 13:36:14 +0100

    Add screen lock script

---
+++ i3/.i3/lock.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+icon="$HOME/.icons/lock.png"
+tmpbg='/tmp/screen-lock.png'
+
+(( $# )) && { icon=$1; }
+
+scrot "$tmpbg"
+convert "$tmpbg" -scale 10% -scale 1000% "$tmpbg"
+convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
+i3lock -u -i "$tmpbg"
